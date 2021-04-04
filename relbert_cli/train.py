a5f63fa42086d3b34ee911aabc52ada98d8da18c
asahi417 <asahi1992ushio@gmail.com>
2021-04-04 11:05:04 +0100

    fix

--- relbert_cli/train.py
+++ relbert_cli/train.py
@@ -23,7 +23,7 @@ def config(parser):
     parser.add_argument('--cache-dir', help='cache directory to store dataset', default=None, type=str)
     parser.add_argument('--num-workers', help='workers for dataloder', default=1, type=int)
     parser.add_argument('--fp16', help='fp16 for training', action='store_true')
-    parser.add_argument('--epoch-save', help='interval to save model weight', default=1, type=int)
+    parser.add_argument('--epoch-save', help='interval to save model weight', default=3, type=int)
     parser.add_argument('--debug', help='log level', action='store_true')
     parser.add_argument('--export-dir', help='directory to export model weight file', default='./ckpt/relbert', type=str)
     # language model
