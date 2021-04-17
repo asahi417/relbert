6ab6c0940fe7f6acc1d87f61a4389084a971bf57
asahi417 <asahi1992ushio@gmail.com>
2021-04-17 11:27:06 +0100

    fix

--- relbert_cli/prompt.py
+++ relbert_cli/prompt.py
@@ -7,7 +7,7 @@ import relbert
 def config(parser):
     # prompt
     parser.add_argument('--method', help='prompting method', default='autoprompt', type=str)
-    parser.add_argument('-k', '--topk', help='top k', default=-1, type=int)
+    parser.add_argument('-k', '--topk', help='top k', default=100, type=int)
     parser.add_argument('--trigger-selection', help='method to select trigger', default='random', type=str)
     parser.add_argument('--n-trigger-i', help='trigger number', default=3, type=int)
     parser.add_argument('--n-trigger-b', help='trigger number', default=1, type=int)
