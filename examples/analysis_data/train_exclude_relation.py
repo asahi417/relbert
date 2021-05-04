81ee1454f6f8f69971ab4f192ef58f4c468b99e7
asahi417 <asahi1992ushio@gmail.com>
2021-05-04 11:58:36 +0100

    fix

--- examples/analysis_data/train_exclude_relation.py
+++ examples/analysis_data/train_exclude_relation.py
@@ -1,4 +1,7 @@
+import logging
 import relbert
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 
 for t, name in zip(['c', 'relbert_output/ckpt/auto_d922/epoch_2', 'relbert_output/ckpt/auto_c932/epoch_2'],
                    ['Manual', 'AutoPrompt', 'P-tuning']):
