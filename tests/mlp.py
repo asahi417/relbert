64ec207b9259e205bda915ca7846320d5bb3cd2e
asahi417 <asahi1992ushio@gmail.com>
2021-05-12 22:26:14 +0100

    fix

--- tests/mlp.py
+++ tests/mlp.py
@@ -11,8 +11,14 @@ y = rng.binomial(n=1, p=0.5, size=N)
 
 
 clf_0 = MLPClassifier(random_state=0)
+clf_0.fit(X, y)
+print(clf_0.coefs_)
 clf_1 = MLPClassifier(random_state=0)
+clf_1.fit(X, y)
+print(clf_1.coefs_)
 clf_2 = MLPClassifier(random_state=0)
+clf_2.fit(X, y)
+print(clf_2.coefs_)
 
 print(cross_val_score(clf_0, X, y, cv=2, scoring='roc_auc'))
 print(cross_val_score(clf_1, X, y, cv=2, scoring='roc_auc'))
