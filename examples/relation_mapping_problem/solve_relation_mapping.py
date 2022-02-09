047032604a07e19c838242f3254ae9f7c274224b
asahi417 <asahi1992ushio@gmail.com>
2022-02-09 20:16:46 +0000

    analogical proportion score

--- examples/relation_mapping_problem/solve_relation_mapping.py
+++ examples/relation_mapping_problem/solve_relation_mapping.py
@@ -1,12 +1,20 @@
 import os
 import json
-from itertools import permutations, product
-from relbert import AnalogyScore, RelBERT
+from itertools import product
+
+import numpy as np
+from munkres import Munkres
+from relbert import AnalogyScore, RelBERT, cosine_similarity
 
 os.makedirs('cache', exist_ok=True)
 
 
-def analogy_score(source_list, target_list, cache_file='tmp.json', model='bert-large-cased', aggregation=min):
+def compute_score(source_list,
+                  target_list,
+                  cache_file='tmp.json',
+                  model='bert-large-cased',
+                  model_type: str = 'analogy_score'):
+    # analogy score configuration
     configs = {
         "roberta-large": {"weight_head": 0.2, "weight_tail": 0.2, "template": 'as-what-same',
                           "positive_permutation": 4, "negative_permutation": 10, "weight_negative": 0.2},
@@ -15,14 +23,29 @@ def analogy_score(source_list, target_list, cache_file='tmp.json', model='bert-l
         "bert-large-cased": {"weight_head": -0.2, "weight_tail": -0.4, "template": 'what-is-to',
                              "positive_permutation": 4, "negative_permutation": 4, "weight_negative": 0.2}
     }
+
+    # compute each assignment score
+    size = len(source_list)
     if os.path.exists(cache_file):
         with open(cache_file) as f:
             model_input = json.load(f)
     else:
-        config = configs[model]
-        scorer = AnalogyScore(model=model)
+        if model_type == 'analogy_score':
+            config = configs[model]
+            scorer = AnalogyScore(model=model)
+
+            def get_score(_query, _options):
+                return scorer.analogy_score(query_word_pair=query, option_word_pairs=options, batch_size=1024, **config)
+
+        elif model_type == 'relbert':
+            relbert_model = RelBERT('asahi417/relbert-roberta-large')
+
+            def get_score(_query, _options):
+                vector = relbert_model.get_embedding([_query] + _options, batch_size=1024)
+                return [cosine_similarity(vector[0], _v) for _v in vector[1:]]
+        else:
+            raise ValueError('unknown type: {}'.format(model_type))
         model_input = {}
-        size = len(source_list)
         for n, (source_n, target_n) in enumerate(product(range(size), range(size))):
             print('\t compute score: {}/{}'.format(n + 1, size*size))
             query = [source_list[source_n], target_list[target_n]]
@@ -34,17 +57,34 @@ def analogy_score(source_list, target_list, cache_file='tmp.json', model='bert-l
                     if target_n == target_pair_n:
                         continue
                     options.append([source_list[source_pair_n], target_list[target_pair_n]])
-            score = scorer.analogy_score(query_word_pair=query, option_word_pairs=options, batch_size=1024,
-                                         **config)
-            key = '-'.join([str(source_n), str(target_n)])
-            model_input[key] = score
+            score = get_score(query, options)
+            model_input['{}-{}'.format(source_n, target_n)] = score
         with open(cache_file, 'w') as f:
             json.dump(model_input, f)
-    print(model_input)
-    # score = [(k, aggregation(v)) for k, v in scores.items()]
-    # pred = int(sorted(score, key=lambda x: x[1], reverse=True)[0][0])
-    # pred_permutation = perms[pred]
-    # return pred_permutation
+
+    # get overall score for each assignment pattern
+    scores = {}
+    assignments = {}
+    m = Munkres()
+    for n, (source_n, target_n) in enumerate(product(range(size), range(size))):
+        # scores for all possible pairs apart from the reference
+        score = model_input['{}-{}'.format(source_n, target_n)]
+        # matrix of assignment cost: source x target
+        matrix = np.array([score[size * i:size * (1 + i)] for i in range(size)])
+        # compute the cheapest assignments and get the overall cost by summing up each cost
+        best_assignment = m.compute(matrix)
+        scores['{}-{}'.format(source_n, target_n)] = np.sum([matrix[a, b] for a, b in best_assignment])
+        # get the best assignment's pairs
+        best_assignment = [[a if a < source_n else a + 1, b if b < target_n else b + 1] for a, b in best_assignment]
+        best_assignment = best_assignment + [[source_n, target_n]]
+        assignments['{}-{}'.format(source_n, target_n)] = sorted(best_assignment, key=lambda st: st[0])
+    # find the reference with the cheapest assignment
+    best_assignment_key = sorted(scores.items(), key=lambda kv: kv[1])[0][0]
+    best_assignment = assignments[best_assignment_key]
+    # get the final order of the target term
+    target_order = [i[1] for i in best_assignment]
+    print(target_order)
+    return target_order
 
 
 if __name__ == '__main__':
@@ -53,17 +93,20 @@ if __name__ == '__main__':
     with open('data.jsonl') as f_reader:
         data = [json.loads(i) for i in f_reader.read().split('\n') if len(i) > 0]
 
+    accuracy = {'analogy_score (roberta)': [], 'relbert': []}
     for data_id, i in enumerate(data):
-        print('Processing {}/{}'.format(data_id + 1, len(data)))
-        pred_as_ro = analogy_score(i['source'], i['target_random'],
-                                   model='roberta-large',
-                                   cache_file='cache/analogy_score.roberta_large.{}.json'.format(data_id))
-        # print(pred_as_ro)
-        # prediction = analogy_score(i['source'], i['target_random'],
-        #                            model='gpt2-xl',
-        #                            cache_file='cache/analogy_score.gpt2_Xl.{}.json'.format(data_id))
-        # prediction = analogy_score(i['source'], i['target_random'],
-        #                            model='bert-large-cased',
-        #                            cache_file='cache/analogy_score.bert_large_cased.{}.json'.format(data_id))
-        print(pred_as_ro)
-        print(i['target'])
+        print('Processing [Analogy Score] {}/{}'.format(data_id + 1, len(data)))
+        pred = compute_score(
+            i['source'], i['target_random'], model_type='analogy_score', model='roberta-large',
+            cache_file='cache/analogy_score.roberta_large.{}.json'.format(data_id))
+        accuracy['analogy_score (roberta)'].append(int(pred == i['target']))
+
+        print('Processing [RelBERT] {}/{}'.format(data_id + 1, len(data)))
+        pred = compute_score(
+            i['source'], i['target_random'], model_type='relbert',
+            cache_file='cache/relbert.roberta_large.{}.json'.format(data_id))
+        accuracy['relbert'].append(int(pred == i['target']))
+
+    print('\nAccuracy')
+    for k, v in accuracy.items():
+        print('\t{}: {}'.format(k, sum(v)/len(v) * 100))
