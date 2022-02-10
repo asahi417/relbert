33ec29c48d7d6e79447e39c99bf1637289f48639
asahi417 <asahi1992ushio@gmail.com>
2022-02-10 14:40:18 +0000

    analogical proportion score

--- examples/concept_net/umap_2d_embedding.py
+++ examples/concept_net/umap_2d_embedding.py
@@ -14,54 +14,62 @@ import seaborn as sns
 from umap import UMAP
 from gensim.models import KeyedVectors
 
-sample_size = 100000
-# load gensim model
-model = KeyedVectors.load_word2vec_format("data/relbert_embedding.bin", binary=True)
-concept_net_processed_file_dir = 'data/conceptnet'
+if not os.path.exists('data/conceptnet_2d_embeddings.npy') or \
+        not os.path.exists('data/conceptnet_2d_embeddings.relation_type.txt'):
+    sample_size = 100000
+    # load gensim model
+    model = KeyedVectors.load_word2vec_format("data/relbert_embedding.bin", binary=True)
+    concept_net_processed_file_dir = 'data/conceptnet'
 
 
-def get_term(arg):
-    return arg.split('/en/')[-1].split('/')[0]
+    def get_term(arg):
+        return arg.split('/en/')[-1].split('/')[0]
 
 
-stats = {}
-for i in glob('{}/*.jsonl'.format(concept_net_processed_file_dir)):
-    relation_type = os.path.basename(i).replace('.jsonl', '')
-    with open(i) as f:
-        tmp = [json.loads(t) for t in f.read().split('\n') if len(t) > 0]
-    stats[relation_type] = len(tmp)
-print('Raw')
-print(stats)
-stats = {k: int(v * sample_size/sum(stats.values())) for k, v in stats.items()}
-print('Down sampled')
-print(stats)
+    stats = {}
+    for i in glob('{}/*.jsonl'.format(concept_net_processed_file_dir)):
+        relation_type = os.path.basename(i).replace('.jsonl', '')
+        with open(i) as f:
+            tmp = [json.loads(t) for t in f.read().split('\n') if len(t) > 0]
+        stats[relation_type] = len(tmp)
+    print('Raw')
+    print(stats)
+    stats = {k: int(v * sample_size/sum(stats.values())) for k, v in stats.items()}
+    print('Down sampled')
+    print(stats)
 
-embeddings = []
-relation_types = []
-for i in glob('{}/*.jsonl'.format(concept_net_processed_file_dir)):
-    relation_type = os.path.basename(i).replace('.jsonl', '')
-    print('relation type: {}'.format(relation_type))
-    with open(i) as f:
-        tmp = [json.loads(t) for t in f.read().split('\n') if len(t) > 0]
+    embeddings = []
+    relation_types = []
+    for i in glob('{}/*.jsonl'.format(concept_net_processed_file_dir)):
+        relation_type = os.path.basename(i).replace('.jsonl', '')
+        print('relation type: {}'.format(relation_type))
+        with open(i) as f:
+            tmp = [json.loads(t) for t in f.read().split('\n') if len(t) > 0]
 
-    # down sample
-    seed(0)
-    shuffle(tmp)
-    tmp = tmp[:stats[relation_type]]
+        # down sample
+        seed(0)
+        shuffle(tmp)
+        tmp = tmp[:stats[relation_type]]
 
-    # get embedding
-    for _tmp in tmp:
-        key = '{}__{}'.format(get_term(_tmp['arg1']), get_term(_tmp['arg2']))
-        embeddings.append(model[key])
-        relation_types.append(relation_type)
+        # get embedding
+        for _tmp in tmp:
+            key = '{}__{}'.format(get_term(_tmp['arg1']), get_term(_tmp['arg2']))
+            embeddings.append(model[key])
+            relation_types.append(relation_type)
 
-relation_type_dict = {v: n for n, v in enumerate(sorted(list(set(relation_types))))}
-data = np.stack(embeddings)  # data x dimension
+    relation_type_dict = {v: n for n, v in enumerate(sorted(list(set(relation_types))))}
+    data = np.stack(embeddings)  # data x dimension
 
-# dimension reduction
-print('UMAP training'.format(data.shape))
-embedding_2d = UMAP().fit_transform(data)
-np.save('data/conceptnet_2d_embeddings.npy', embedding_2d)
+    # dimension reduction
+    print('UMAP training'.format(data.shape))
+    embedding_2d = UMAP().fit_transform(data)
+    np.save('data/conceptnet_2d_embeddings.npy', embedding_2d)
+    with open('data/conceptnet_2d_embeddings.relation_type.txt', 'w') as f:
+        f.write('\n'.join(relation_types))
+else:
+    embedding_2d = np.load('data/conceptnet_2d_embeddings.npy')
+    with open('data/conceptnet_2d_embeddings.relation_type.txt') as f:
+        relation_types = [i for i in f.read().split('\n') if len(i) > 0]
 # print(embedding_2d.shape)
 # plt.scatter(
 #     embedding_2d[:, 0],
