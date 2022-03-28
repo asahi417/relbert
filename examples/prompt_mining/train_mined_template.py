bdd06ac3bac6b677873d24a020a7326af490a1d3
asahi417 <asahi1992ushio@gmail.com>
2022-03-28 12:15:39 +0100

    update

--- examples/prompt_mining/train_mined_template.py
+++ examples/prompt_mining/train_mined_template.py
@@ -4,32 +4,19 @@ import subprocess
 from glob import glob
 
 
-def exe_shell(command: str, exported_file: str = None):
-    """ Execute shell command """
-    logging.info("execute `{}`".format(command))
-    try:
-        args = dict(stderr=subprocess.STDOUT, shell=True, timeout=600, universal_newlines=True)
-        log = subprocess.check_output(command, **args)
-        logging.info("log\n{}".format(log))
-    except subprocess.CalledProcessError as exc:
-        if exported_file and os.path.exists(exported_file):
-            # clear possibly broken file out
-            os.system('rm -rf {}'.format(exported_file))
-        raise ValueError("fail to execute command `{}`:\n {}\n {}".format(command, exc.returncode, exc.output))
-
-
 def reader(_file):
     with open(_file) as f:
         return [i for i in f.read().split('\n') if len(i) > 0]
 
 
 # get prompt
-prompts = {}
-for i in glob('output/template.*.*10.csv'):
-    with open(i) as f:
-        prompts = f.read().split('\n')
-    for p in prompts:
-        if len(p) == 0:
-            continue
-        output = os.path.basename(i).replace('.csv', '')
-        exe_shell(f'relbert-train -m roberta-large -n -p -s --export ./ckpt/{output} --custom-template "{p}"')
+with open('./run.sh', 'w') as f:
+    prompts = {}
+    for i in glob('output/template.*.*10.csv'):
+        with open(i) as f:
+            prompts = f.read().split('\n')
+        for p in prompts:
+            if len(p) == 0:
+                continue
+            output = os.path.basename(i).replace('.csv', '')
+            f.write(f'relbert-train -m roberta-large -n -p -s --export ./ckpt/{output} --custom-template "{p}" \n')
