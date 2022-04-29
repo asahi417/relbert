efaa6e013079a92903ee7d7e1dce598dcdb9590d
asahi417 <asahi1992ushio@gmail.com>
2022-04-29 13:00:21 +0100

    update

--- examples/relation_mapping_problem/relation_mapping_test.py
+++ examples/relation_mapping_problem/relation_mapping_test.py
@@ -1,6 +1,8 @@
 import os
 import json
 from itertools import permutations
+
+import pandas as pd
 from tqdm import tqdm
 from numpy import dot
 from numpy.linalg import norm
@@ -22,12 +24,15 @@ if __name__ == '__main__':
     accuracy_full = {}
     for m in ['relbert', 'fasttext_cc']:
         accuracy = []
+        sims_full = []
+        perms_full = []
         for data_id, _data in enumerate(data):
             print(f'[{m}]: {data_id}/{len(data)}')
             with open(f'embeddings/{m}.vector.{data_id}.json') as f:
                 embedding_dict = json.load(f)
             sim_file = f'embeddings/{m}.sim.{data_id}.json'
             sim = {}
+            sim_flag = {}
             if os.path.exists(sim_file):
                 with open(sim_file) as f:
                     sim = json.load(f)
@@ -46,9 +51,28 @@ if __name__ == '__main__':
                         )
                         with open(sim_file, 'w') as f_writer:
                             json.dump(sim, f_writer)
+                    if target[id_x] == tmp_target[id_x] and target[id_y] == tmp_target[id_y]:
+                        sim_flag[_id] = True
+                    else:
+                        sim_flag[_id] = False
                     list_sim.append(sim[_id])
                 perms.append({'target': tmp_target, 'similarity_mean': mean(list_sim)})
+            sims_full.extend([{'pair': k, 'sim': v, 'is_analogy': sim_flag[k], 'data_id': data_id}
+                              for k, v in sim.items()])
             pred = sorted(perms, key=lambda _x: _x['similarity_mean'], reverse=True)
             accuracy.extend([t == p for t, p in zip(target, pred[0]['target'])])
+            tmp = [i for i in perms if list(i['target']) == target]
+            assert len(tmp) == 1, perms
+            perms_full.append({
+                'source': source,
+                'true': target, 'pred': pred[0]['target'], 'accuracy': list(pred[0]['target']) == target,
+                'similarity': pred[0]['similarity_mean'], 'similarity_true': tmp[0]['similarity_mean']
+            })
+        pd.DataFrame(sims_full).to_csv(f'./output/stats.sim.{m}.csv')
+        pd.DataFrame(perms_full).to_csv(f'./output/stats.breakdown.{m}.csv')
+
         accuracy_full[m] = mean(accuracy)
+
     print(json.dumps(accuracy_full, indent=4))
+    with open('output/result.json', 'w') as f:
+        json.dump(accuracy_full, f)
