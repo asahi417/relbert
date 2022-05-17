a739d69c25ae679a805f54bb7b4c0b528d103704
asahi417 <asahi1992ushio@gmail.com>
2022-05-17 22:44:57 +0100

    update

--- relbert/trainer.py
+++ relbert/trainer.py
@@ -14,6 +14,12 @@ from .data import get_training_data
 from .util import get_linear_schedule_with_warmup, fix_seed
 
 
+def stack_sum(_list):
+    if len(_list) == 0:
+        return 0
+    return torch.sum(torch.stack(_list))
+
+
 class Trainer:
 
     def __init__(self,
@@ -155,9 +161,8 @@ class Trainer:
                                 dist = torch.exp(cos_2d(embedding_p[i].unsqueeze(0), embedding_p) / tau)
                                 # input([d for n, d in enumerate(dist) if rank[n] <= rank[i]])
                                 # input([d for n, d in enumerate(dist) if rank[n] > rank[i]])
-                                nume_p = torch.sum(torch.stack([d for n, d in enumerate(dist) if rank[n] >= rank[i]]))
-                                input([d for n, d in enumerate(dist) if rank[n] < rank[i]])
-                                deno_p = torch.sum(torch.stack([d for n, d in enumerate(dist) if rank[n] < rank[i]]))
+                                nume_p = stack_sum([d for n, d in enumerate(dist) if rank[n] >= rank[i]])
+                                deno_p = stack_sum([d for n, d in enumerate(dist) if rank[n] < rank[i]])
                                 loss.append(- torch.log(nume_p / (deno_p + deno_n)))
                         elif self.config['loss_function'] == 'nce_logout':
                             for i in range(batch_size_positive):
@@ -176,7 +181,7 @@ class Trainer:
                                 loss.append(- torch.log(logit_p/(logit_p + deno_n)))
                         else:
                             raise ValueError(f"unknown loss function {self.config['loss_function']}")
-                        loss = torch.sum(torch.stack(loss))
+                        loss = stack_sum(loss)
                         loss.backward()
                         total_loss.append(loss.cpu().item())
                         self.optimizer.step()
