24a325a9164cc8ec2461dfa354f189a40e77f0ae
asahi417 <asahi1992ushio@gmail.com>
2022-07-15 14:20:24 +0100

    update

--- examples/relbert_training/training.sh
+++ examples/relbert_training/training.sh
@@ -3,20 +3,34 @@
 # - Small Temperature.
 
 relbert_training() {
-  MODEL=${1}
-  MODE=${2}
-  LOSS=${3}
-  EPOCH=${4}
-  GRAD=${5}
+  MODE=${1}
+  LOSS=${2}
+  MODEL='roberta-large'
+  EXPORT_FILE="relbert_output/eval/accuracy.csv"
+  EPOCH=50
+  GRAD=8
   LR=0.000005
   TEMP=0.05
+  TEMP_MIN=0.01
   NSAMPLE=640
-  relbert-train -m "${MODEL}" --mode "${MODE}" -l "${LOSS}" -e "${EPOCH}" -b 128 --n-sample ${NSAMPLE} \
-    --export "relbert_output/models/d.${LOSS}.${MODE}.${MODEL}.${LR}.${GRAD}.${TEMP}.${NSAMPLE}" \
-    -t "I wasn’t aware of this relationship, but I just read in the encyclopedia that <subj> is the <mask> of <obj>" \
-    --lr "${LR}" -g "${GRAD}" --temperature-nce-constant "${TEMP}"
-  relbert-eval -c "relbert_output/models/*/epoch*" --export-file "relbert_output/eval/accuracy.analogy5.csv" --type "analogy"
+  CKPT="relbert_output/models/d.${LOSS}.${MODE}.${MODEL}.${LR}.${GRAD}.${TEMP}.${NSAMPLE}"
+  relbert-train -m "${MODEL}" --mode "${MODE}" -l "${LOSS}" -e "${EPOCH}" -b 128 --n-sample ${NSAMPLE} --export "${CKPT}" --lr "${LR}" -g "${GRAD}" \
+    --temperature-nce-constant "${TEMP}" \
+    --temperature-nce-max "${TEMP}"\
+    --temperature-nce-min "${TEMP_MIN}" \
+    -t "I wasn’t aware of this relationship, but I just read in the encyclopedia that <subj> is the <mask> of <obj>"
+  relbert-eval -c "${CKPT}/epoch*" --export-file ${EXPORT_FILE} --type "analogy" --return-validation-loss
 }
 
-relbert_training 'roberta-large' 'mask' "nce_logout" 100 8
-#relbert_training 'roberta-large' 'mask' "nce_rank" 15 8
+relbert_training 'mask' "nce_logout"
+relbert_training 'mask' "nce_rank"
+
+#df = pd.read_csv('relbert_output/eval/accuracy.analogy5.csv', index_col=0)
+#df['epoch'] = [int(i.rsplit('/', 1)[-1].replace('epoch_', '')) for i in df['model']]
+#df = df[df['epoch'] < 50]
+#out = []
+#for data in df.data.unique():
+#  df_ = df[df.data == data].sort_values(by=['accuracy/test']).tail(1)
+#  out.append(df_)
+#  print(data, df_.to_dict())
+#  print()
