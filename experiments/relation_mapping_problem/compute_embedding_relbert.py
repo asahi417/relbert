7458550e7eeb1c9c84dc90470862201eb1c12687
asahi417 <asahi1992ushio@gmail.com>
2022-08-11 12:26:36 +0100

    update README

--- experiments/relation_mapping_problem/compute_embedding_relbert.py
+++ experiments/relation_mapping_problem/compute_embedding_relbert.py
@@ -1,18 +1,20 @@
 import os
 import json
 import argparse
+import logging
 from itertools import permutations
 from os.path import join as pj
 from relbert import RelBERT
 from datasets import load_dataset
 
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 data = [i for i in load_dataset("relbert/relation_mapping")["test"]]
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='RelBERT evaluation on analogy/relation classification')
     parser.add_argument('-m', '--model', help='model name', default="relbert/relbert-roberta-large", type=str)
-    parser.add_argument('-b', '--batch', help='batch', default=64, type=str)
+    parser.add_argument('-b', '--batch-size', help='batch', default=64, type=str)
     opt = parser.parse_args()
 
     model_alias = os.path.basename(opt.model)
@@ -20,7 +22,7 @@ if __name__ == '__main__':
     os.makedirs(pj('embeddings', model_alias), exist_ok=True)
 
     for data_id, _data in enumerate(data):
-        print(f'[{model_alias}]: {data_id}/{len(data)}')
+        logging.info(f'[{model_alias}]: {data_id}/{len(data)}')
         cache_file = pj('embeddings', model_alias, f'vector.{data_id}.json')
         embedding_dict = {}
         if os.path.exists(cache_file):
@@ -36,5 +38,6 @@ if __name__ == '__main__':
                     inputs.append([x, y])
         vector = model.get_embedding(inputs, batch_size=opt.batch_size)
         embedding_dict.update({i: v for i, v in zip(inputs_id, vector)})
+        logging.info(f'update cache file {cache_file}')
         with open(cache_file, 'w') as f_writer:
             json.dump(embedding_dict, f_writer)
