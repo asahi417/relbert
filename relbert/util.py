3dcf54b6892b8c0a55fce4d60ae46c215a511edf
asahi417 <asahi1992ushio@gmail.com>
2022-09-23 01:30:00 +0100

    init

--- relbert/util.py
+++ relbert/util.py
@@ -106,35 +106,37 @@ class NCELoss:
                 loss.append(- torch.log(logit_p / (logit_p + deno_n)))
             loss = stack_sum(loss)
         elif self.loss_function == 'triplet':
-            print(embedding_p.shape, embedding_n.shape)
-            d_positive = torch.sum((embedding_p.unsqueeze(0) - embedding_p.unsqueeze(1)) ** 2, -1) ** 0.5
-            d_negative = torch.sum((embedding_n.unsqueeze(0) - embedding_p.unsqueeze(1)) ** 2, -1) ** 0.5
-            for i in range(batch_size_positive):
-                for p in range(batch_size_positive):
-                    if i != p:
-                        loss.append(
-                            torch.sum(torch.clip(d_positive[i, p].unsqueeze(0) - d_negative[i] - self.margin,
-                                                 min=self.boundary))
-                            )
-            loss = stack_sum(loss)
-            print(d_positive)
-            print(d_negative)
+            # print(embedding_p.shape, embedding_n.shape)
+            # d_positive = torch.sum((embedding_p.unsqueeze(0) - embedding_p.unsqueeze(1)) ** 2, -1) ** 0.5
+            # d_negative = torch.sum((embedding_n.unsqueeze(0) - embedding_p.unsqueeze(1)) ** 2, -1) ** 0.5
+            # for i in range(batch_size_positive):
+            #     for p in range(batch_size_positive):
+            #         if i != p:
+            #             loss.append(
+            #                 torch.sum(torch.clip(d_positive[i, p].unsqueeze(0) - d_negative[i] - self.margin,
+            #                                      min=self.boundary))
+            #                 )
+            # loss = stack_sum(loss)
+            # print(d_positive)
+            # print(d_negative)
             # matrix = torch.sum(torch.clip(d_positive.unsqueeze(-1) - d_negative.unsqueeze(-2) - self.margin, min=self.boundary), -1)
             # print(matrix)
             # loss = torch.sum(matrix.fill_diagonal_(0))
+
+
+            for i in range(batch_size_positive):
+                distance_positive = []
+                distance_negative = []
+                for p in range(batch_size_positive):
+                    if i != p:
+                        distance_positive.append(torch.sum((embedding_p[i] - embedding_p[p]) ** 2, -1) ** 0.5)
+                for n in range(len(embedding_n)):
+                    distance_negative.append(torch.sum((embedding_p[i] - embedding_n[n]) ** 2, -1) ** 0.5)
+                for d_p, d_n in product(distance_positive, distance_negative):
+                    loss.append(torch.sum(torch.clip(d_p - d_n - self.margin, min=self.boundary)))
+            loss = stack_sum(loss)
             print(loss)
             input()
-
-            # for i in range(batch_size_positive):
-            #     distance_positive = []
-            #     distance_negative = []
-            #     for p in range(batch_size_positive):
-            #         if i != p:
-            #             distance_positive.append(torch.sum((embedding_p[i] - embedding_p[p]) ** 2, -1) ** 0.5)
-            #     for n in range(len(embedding_n)):
-            #         distance_negative.append(torch.sum((embedding_p[i] - embedding_n[n]) ** 2, -1) ** 0.5)
-            #     for d_p, d_n in product(distance_positive, distance_negative):
-            #         loss.append(torch.sum(torch.clip(d_p - d_n - self.margin, min=self.boundary)))
         else:
             raise ValueError(f"unknown loss function {self.loss_function}")
         if self.linear is not None:
