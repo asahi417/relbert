520dedbc2cca2270f0030236ce6ab601a661c906
asahi417 <asahi1992ushio@gmail.com>
2023-01-24 14:29:46 +0000

    init

--- relbert/__init__.py
+++ relbert/__init__.py
@@ -3,3 +3,4 @@ from .trainer import Trainer
 from .evaluation.analogy_questions import evaluate_analogy
 from .evaluation.lexical_relation_classification import evaluate_classification
 from .evaluation.relation_mapping import evaluate_relation_mapping
+from .evaluation.analogy_questions import cosine_similarity, euclidean_distance
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '2.0.1'
+VERSION = '2.0.2'
 
 with open('README.md', 'r') as f:
     readme = f.read()
