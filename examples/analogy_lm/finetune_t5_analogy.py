c8ec8a56d227df80a33d772f49afc85bbd870b3e
asahi417 <asahi1992ushio@gmail.com>
2023-02-12 21:31:24 +0000

    update

--- examples/analogy_lm/finetune_t5_analogy.py
+++ examples/analogy_lm/finetune_t5_analogy.py
@@ -1,12 +1,15 @@
 import argparse
+import os
+import json
 import logging
-from random import seed, randint
-from itertools import permutations
+from itertools import permutations, chain
+
+import pandas as pd
 import torch
 import transformers
 from datasets import load_dataset
+from lmppl import EncoderDecoderLM
 from huggingface_hub import create_repo
-
 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
 parser = argparse.ArgumentParser(description='Fine-tuning T5 on analogy generation.')
@@ -16,6 +19,7 @@ parser.add_argument('-s', '--random-seed', default=42, type=int)
 parser.add_argument('-e', '--epoch', default=5, type=int)
 parser.add_argument('-l', '--lr', default=1e-4, type=float)
 parser.add_argument('-b', '--batch-size', default=32, type=int)
+parser.add_argument('--batch-size-eval', default=32, type=int)
 parser.add_argument('-d', '--data', default='relbert/semeval2012_relational_similarity', type=str)
 parser.add_argument('--skip-train', help='', action='store_true')
 parser.add_argument('--push-to-hub', help='', action='store_true')
@@ -25,13 +29,9 @@ opt = parser.parse_args()
 ##########
 # Config #
 ##########
-task_prefix = 'generate analogy: '
+task_prefix = 'generate analogy:'
 template_header = "<subj-a> is to <obj-a>"
-template_join = "what"
 template_footer = "<subj-b> is to <obj-b>"
-instruction_header = "Which one of the following is an analogy?"
-instruction_footer = "The correct answer is"
-
 
 ##############
 # Load Model #
@@ -49,7 +49,7 @@ logging.info(f'language model ({opt.model}) running on {model.device}')
 # Dataset Preparation #
 #######################
 def encode(x, y):
-    model_inputs = tokenizer(x, truncation=True)
+    model_inputs = tokenizer(f"{task_prefix} {x}", truncation=True)
     model_inputs['labels'] = tokenizer(text_target=y, truncation=True)['input_ids']
     return model_inputs
 
@@ -60,36 +60,56 @@ tokenized_dataset = []
 for _, g in df.groupby("relation_type"):
     positives = [i.tolist() for i in g['positives'].values[0].tolist()]
     tokenized_dataset += [encode(
-        task_prefix + template_header.replace('<subj-a>', h_a).replace('<obj-a>', t_a),
+        template_header.replace('<subj-a>', h_a).replace('<obj-a>', t_a),
         template_footer.replace('<subj-b>', h_b).replace('<obj-b>', t_b)) for
         (h_a, t_a), (h_b, t_b) in permutations(positives, 2)]
 
+if not opt.skip_train:
+    ##################
+    # Model Training #
+    ##################
+    training_args = transformers.Seq2SeqTrainingArguments(
+        per_device_train_batch_size=opt.batch_size,
+        warmup_steps=0,
+        weight_decay=0.01,
+        learning_rate=opt.lr,
+        num_train_epochs=opt.epoch,
+        output_dir=f'{opt.output_dir}/runs/',
+        logging_dir=f'{opt.output_dir}/logging/',
+        logging_steps=100,
+        evaluation_strategy="no",
+        seed=opt.random_seed
+    )
+    trainer = transformers.Seq2SeqTrainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset,
+        data_collator=transformers.DataCollatorForSeq2Seq(tokenizer, model=model)
+    )
+    trainer.train()
+    trainer.save_model(f"{opt.output_dir}/model")
+    tokenizer.save_pretrained(f"{opt.output_dir}/model")
+assert os.path.exists(f"{opt.output_dir}/model")
 
-##################
-# Model Training #
-##################
-training_args = transformers.Seq2SeqTrainingArguments(
-    per_device_train_batch_size=opt.batch_size,
-    warmup_steps=0,
-    weight_decay=0.01,
-    learning_rate=opt.lr,
-    num_train_epochs=opt.epoch,
-    output_dir=f'{opt.output_dir}/runs/',
-    logging_dir=f'{opt.output_dir}/logging/',
-    logging_steps=100,
-    evaluation_strategy="no",
-    seed=opt.random_seed
-)
-trainer = transformers.Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=tokenized_dataset,
-    data_collator=transformers.DataCollatorForSeq2Seq(tokenizer, model=model)
-)
-trainer.train()
-trainer.save_model(f"{opt.output_dir}/model")
-tokenizer.save_pretrained(f"{opt.output_dir}/model")
-
+####################
+# Model Validation #
+####################
+data_valid = load_dataset(opt.data, split='validation')
+query = [f"{task_prefix} {template_header.replace('<subj-a>', i[0][0]).replace('<obj-a>', i[0][1])}" for i in data_valid['positives']]
+gold = [template_footer.replace('<subj-b>', i[1][0]).replace('<obj-b>', i[1][1]) for i in data_valid['positives']]
+choice = [[template_footer.replace('<subj-b>', i[0]).replace('<obj-b>', i[1]) for i in l] for l in data_valid['negatives']]
+scorer = EncoderDecoderLM(f"{opt.output_dir}/model")
+# get score for gold answer
+gold_score = scorer.get_perplexity(input_texts=query, output_texts=gold, batch=opt.batch_size_eval)
+# get score for the other choices
+choice_flat = list(chain(*choice))
+query_flat = list(chain(*[[q] * len(c) for q, c in zip(choice, query)]))
+choice_score = scorer.get_perplexity(
+    input_texts=query_flat,
+    output_texts=choice_flat,
+    batch=opt.batch_size_eval)
+index = list(chain(*[[n] * len(c) for n, c in enumerate(choice)]))
+df = pd.DataFrame([{i: s} for i, s in zip(index, choice_score)])
 # if opt.push_to_hub:
 #     assert opt.hf_organization is not None, f'specify hf organization `--hf-organization`'
 #     assert opt.model_alias is not None, f'specify hf organization `--model-alias`'
