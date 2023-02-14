d2195138332bbf311a8824432204c027743c7c28
asahi417 <asahi1992ushio@gmail.com>
2023-02-14 00:01:11 +0000

    update

--- examples/analogy_lm/analogy_prompt.py
+++ examples/analogy_lm/analogy_prompt.py
@@ -71,8 +71,8 @@ language_models.update({
 
 # Add MLM
 language_models.update({
-    "microsoft/deberta-v2-xxlarge": [lmppl.MaskedLM, 32],  # 1.5B
-    "microsoft/deberta-v2-xlarge": [lmppl.MaskedLM, 32],  # 900M
+    "microsoft/deberta-v2-xxlarge": [lmppl.MaskedLM, 64],  # 1.5B
+    "microsoft/deberta-v2-xlarge": [lmppl.MaskedLM, 64],  # 900M
     "microsoft/deberta-v3-large": [lmppl.MaskedLM, 128],  # 434M
     "microsoft/deberta-v3-base": [lmppl.MaskedLM, 256],  # 184M
     "microsoft/deberta-v3-small": [lmppl.MaskedLM, 256],  # 142M
