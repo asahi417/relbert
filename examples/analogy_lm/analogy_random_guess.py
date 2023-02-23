e311ec642cfb7dbdfba281bc41fd4eb3e03bcd15
asahi417 <asahi1992ushio@gmail.com>
2023-02-23 10:47:42 +0000

    fix

---
+++ examples/analogy_lm/analogy_random_guess.py
@@ -0,0 +1,9 @@
+from statistics import mean
+from datasets import load_dataset
+
+types = ["sat_full", "u2", "u4", "bats", "google", "t_rex_relational_similarity", "nell_relational_similarity", "conceptnet_relational_similarity"]
+output = {}
+for t in types:
+    data = load_dataset("relbert/analogy_questions", t, split="test")
+    output[t] = mean([1/len(i) for i in data['choice']])
+print(output)
--- examples/analogy_lm/format_result.py
+++ examples/analogy_lm/format_result.py
@@ -3,6 +3,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 os.makedirs('results/figures', exist_ok=True)
+random_guess = {
+    'sat_full': 0.2,
+    'u2': 0.23589181286549707,
+    'u4': 0.24205246913580247,
+    'bats': 0.25,
+    'google': 0.25,
+    't_rex_relational_similarity': 0.020833333333333332,
+    'nell_relational_similarity': 0.14285714285714285,
+    'conceptnet_relational_similarity': 0.058823529411764705
+}
 model_size = {
     # "roberta-base": [110, "RoBERTa"],
     # "roberta-large": [355, "RoBERTa"],
@@ -41,20 +51,71 @@ model_size = {
 
 df = pd.read_csv('results/full_result.prompt.csv')
 df['Accuracy'] = df.pop('accuracy')  # * 100
-df = df[[i not in ['sat_metaphor', 'sat'] for i in df['data']]]
+df = df[[i not in ['sat'] for i in df['data']]]
+df['data'] = [f"{d}_{p}" if d == 'sat_metaphor' else d for d, p in zip(df['data'], df['prefix'])]
 df = df[[i in model_size for i in df['model']]]
 df.pop('prefix')
+df['lm'] = [model_size[i][1] for i in df['model']]
+df['Model Size'] = [model_size[i][0] * 1000000 for i in df['model']]
+lms = ['GPT-2', 'GPT-J', 'OPT', 'OPT-IML', 'T5', 'Flan-T5', 'Flan-T5 (FT)']
+colors = ['red', 'blue', 'green', 'orange', 'purple', 'gray', 'black']
+styles = ['o-', 'o--', 'o:', 's-', 's--', 's:', '^-', '^--', '^:']
+
+# OVERALL RESULT
+out = df.pivot_table(index='Model Size', columns='lm', aggfunc='mean')
+out.columns = [i[1] for i in out.columns]
+out = out.reset_index()
+tmp = out[['Model Size', lms[0]]].dropna().reset_index()
+tmp['Accuracy'] = tmp[lms[0]]
+ax = tmp.plot.line(y='Accuracy', x='Model Size', color=colors[0], style=styles[0], label=lms[0], logx=True)
+for n, c in enumerate(lms[1:]):
+    tmp = out[['Model Size', c]].dropna().reset_index()
+    tmp['Accuracy'] = tmp[c]
+    tmp.plot.line(y='Accuracy', x='Model Size', ax=ax, color=colors[n+1], style=styles[n+1], label=c, logx=True)
+plt.grid()
+plt.tight_layout()
+plt.savefig(f"results/figures/curve.average.png", bbox_inches="tight", dpi=600)
+
+
+# RESULT ON 5 ANALOGIES
+_df = df[[i in ['sat_full', 'u2', 'u4', 'bats', 'google'] for i in df['data']]]
+out = _df.pivot_table(index='Model Size', columns='lm', aggfunc='mean')
+out.columns = [i[1] for i in out.columns]
+out = out.reset_index()
+tmp = out[['Model Size', lms[0]]].dropna().reset_index()
+tmp['Accuracy'] = tmp[lms[0]]
+ax = tmp.plot.line(y='Accuracy', x='Model Size', color=colors[0], style=styles[0], label=lms[0], logx=True)
+for n, c in enumerate(lms[1:]):
+    tmp = out[['Model Size', c]].dropna().reset_index()
+    tmp['Accuracy'] = tmp[c]
+    tmp.plot.line(y='Accuracy', x='Model Size', ax=ax, color=colors[n+1], style=styles[n+1], label=c, logx=True)
+plt.grid()
+plt.tight_layout()
+plt.savefig(f"results/figures/curve.average_5.png", bbox_inches="tight", dpi=600)
+
+
+# RESULT ON ENTITY ANALOGIES (NELL and TREX)
+_df = df[[i in ['nell_relational_similarity', 't_rex_relational_similarity'] for i in df['data']]]
+out = _df.pivot_table(index='Model Size', columns='lm', aggfunc='mean')
+out.columns = [i[1] for i in out.columns]
+out = out.reset_index()
+tmp = out[['Model Size', lms[0]]].dropna().reset_index()
+tmp['Accuracy'] = tmp[lms[0]]
+ax = tmp.plot.line(y='Accuracy', x='Model Size', color=colors[0], style=styles[0], label=lms[0], logx=True)
+for n, c in enumerate(lms[1:]):
+    tmp = out[['Model Size', c]].dropna().reset_index()
+    tmp['Accuracy'] = tmp[c]
+    tmp.plot.line(y='Accuracy', x='Model Size', ax=ax, color=colors[n+1], style=styles[n+1], label=c, logx=True)
+plt.grid()
+plt.tight_layout()
+plt.savefig(f"results/figures/curve.average_entity.png", bbox_inches="tight", dpi=600)
+
+
+# RESULT ON INDIVIDUAL ANALOGIES
 for data, g in df.groupby('data'):
-    output = {}
-    g['lm'] = [model_size[i][1] for i in g['model']]
-    g['Model Size'] = [model_size[i][0] * 1000000 for i in g['model']]
     out = g.pivot_table(index='Model Size', columns='lm', aggfunc='mean')
     out.columns = [i[1] for i in out.columns]
     out = out.reset_index()
-    lms = ['GPT-2', 'GPT-J', 'OPT', 'OPT-IML', 'T5', 'Flan-T5', 'Flan-T5 (FT)']
-    colors = ['red', 'blue', 'green', 'orange', 'purple', 'gray', 'black']
-    styles = ['o-', 'o--', 'o:', 's-', 's--', 's:', '^-', '^--', '^:']
-
     tmp = out[['Model Size', lms[0]]].dropna().reset_index()
     tmp['Accuracy'] = tmp[lms[0]]
     ax = tmp.plot.line(y='Accuracy', x='Model Size', color=colors[0], style=styles[0], label=lms[0], logx=True)
