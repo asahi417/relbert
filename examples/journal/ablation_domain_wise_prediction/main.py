9857e0f9dbf13493ab97132ae13533e53cd14179
asahi417 <asahi1992ushio@gmail.com>
2023-05-07 22:25:53 +0100

    init

--- examples/journal/ablation_domain_wise_prediction/main.py
+++ examples/journal/ablation_domain_wise_prediction/main.py
@@ -22,25 +22,27 @@ data_names = {
 }
 
 
-model = RelBERT("relbert/relbert-roberta-large")
+for size in ['base', 'large']:
+    model = RelBERT(f"relbert/relbert-roberta-{size}")
 
-for i in data_names.keys():
-    if not os.path.exists(f"results/{i}.csv"):
+    for i in data_names.keys():
+        if not os.path.exists(f"results/{i}.{size}.csv"):
 
-        data = load_dataset("relbert/analogy_questions", i, split='test')
-        v_stem = model.get_embedding(data['stem'], batch_size=1024)
+            data = load_dataset("relbert/analogy_questions", i, split='test')
+            v_stem = model.get_embedding(data['stem'], batch_size=1024)
 
-        choice_flat = []
-        choice_index = []
-        for n, c in enumerate(data['choice']):
-            choice_flat += c
-            choice_index += [n] * len(c)
-        v_choice_flat = model.get_embedding(choice_flat, batch_size=1024)
+            choice_flat = []
+            choice_index = []
+            for n, c in enumerate(data['choice']):
+                choice_flat += c
+                choice_index += [n] * len(c)
+            v_choice_flat = model.get_embedding(choice_flat, batch_size=1024)
 
-        v_choice = [[v for v, c_i in zip(v_choice_flat, choice_index) if c_i == _i] for _i in range(len(data['choice']))]
-        assert len(v_choice) == len(v_stem)
-        sims = [[cosine_similarity(_c, s) for _c in c] for c, s in zip(v_choice, v_stem)]
-        pred = [s.index(max(s)) for s in sims]
-        acc = [int(a == p) for a, p in zip(data['answer'], pred)]
-        df = pd.DataFrame([data['prefix'], acc], index=['prefix', 'accuracy']).T
-        df.to_csv(f"results/{i}.csv")
+            v_choice = [[v for v, c_i in zip(v_choice_flat, choice_index) if c_i == _i] for _i in range(len(data['choice']))]
+            assert len(v_choice) == len(v_stem)
+            sims = [[cosine_similarity(_c, s) for _c in c] for c, s in zip(v_choice, v_stem)]
+            pred = [s.index(max(s)) for s in sims]
+            acc = [int(a == p) for a, p in zip(data['answer'], pred)]
+            df = pd.DataFrame([data['prefix'], acc], index=['prefix', 'accuracy']).T
+            df.to_csv(f"results/{i}.{size}.csv")
+        df = pd.read_csv(f"results/{i}.{size}.csv", index_col=0)
